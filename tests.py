11aa289bcde1aabc6a374bfd502965de00c8f47b
Fedor Serkov <serkovfb@gmail.com>
2021-01-15 19:53:41 +1000

    test_initial failed

--- tests.py
+++ tests.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 import unittest
 
 
 class TestDiaryBase(unittest.TestCase):
     def test_initial(self):
-        pass
+        db = Database()
+        dt = datetime(year=2021, month=1, day=15, hour=19, minute=46),
+        text_in = 'This is test text.'
+
+        db.add(dt, text_in)
+        text_out = db.get(dt)
+
+        self.assertEqual(text_out, text_in)
 
 
 if __name__ == '__main__':
