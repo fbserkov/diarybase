e1842e7bdb4491ceee523b78badba6309b1c75d1
Fedor Serkov <serkovfb@gmail.com>
2021-01-15 21:25:16 +1000

    test_save_and_load failed

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 import unittest
+from datetime import datetime
 
 from database import Database
 
@@ -14,6 +14,21 @@ class TestDiaryBase(unittest.TestCase):
         text_out = db.get(dt)
 
         self.assertEqual(text_out, text_in)
+
+    def test_save_and_load(self):
+        db = Database()
+        dt = datetime(year=2021, month=1, day=15, hour=21, minute=13)
+        text_save = 'Test text.'
+        db.set(dt, text_save)
+
+        filename = 'test.db'
+        db.save(filename)
+        del db
+        db = Database()
+        db.load(filename)
+        text_load = db.get(dt)
+
+        self.assertEqual(text_save, text_load)
 
 
 if __name__ == '__main__':
