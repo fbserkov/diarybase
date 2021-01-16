1726b5d2a7b5f99051593463310cb41713757129
Fedor Serkov <serkovfb@gmail.com>
2021-01-16 10:57:05 +1000

    Parameter filename in the Database class must be required (create a test db object in a setUp function).

--- database.py
+++ database.py
@@ -3,19 +3,17 @@ from pickle import dump, load
 
 
 class Database:
-    def __init__(self, filename=None):
+    def __init__(self, filename):
         self._filename = filename
         self._records = {}
 
     def save(self):
-        if self._filename:
-            with open(self._filename, 'wb') as file:
-                dump(self._records, file)
+        with open(self._filename, 'wb') as file:
+            dump(self._records, file)
 
     def load(self):
-        if self._filename:
-            with open(self._filename, 'rb') as file:
-                self._records = load(file)
+        with open(self._filename, 'rb') as file:
+            self._records = load(file)
 
     def set(self, dt: datetime, text):
         if dt in self._records:
--- tests.py
+++ tests.py
@@ -5,38 +5,38 @@ from database import Database
 
 
 class TestDiaryBase(unittest.TestCase):
+    def setUp(self) -> None:
+        self._filename = 'test.db'
+        self._db = Database(self._filename)
+
     def test_initial(self):
-        db = Database()
         dt = datetime(year=2021, month=1, day=15, hour=19, minute=46)
         text_in = 'Test text.'
 
-        db.set(dt, text_in)
-        text_out = db.get(dt)
+        self._db.set(dt, text_in)
+        text_out = self._db.get(dt)
 
         self.assertEqual(text_out, text_in)
 
     def test_save_and_load(self):
-        filename = 'test.db'
-        db = Database(filename)
         dt = datetime(year=2021, month=1, day=15, hour=21, minute=13)
         text_save = 'Test text.'
-        db.set(dt, text_save)
+        self._db.set(dt, text_save)
 
-        db.save()
-        db = Database(filename)
+        self._db.save()
+        db = Database(self._filename)
         db.load()
         text_load = db.get(dt)
 
         self.assertEqual(text_save, text_load)
 
     def test_set_to_existing_datetime(self):
-        db = Database()
         dt = datetime(year=2021, month=1, day=15, hour=21, minute=46)
         text = 'Test text.'
 
-        db.set(dt, text)
+        self._db.set(dt, text)
         with self.assertRaises(Exception):
-            db.set(dt, text)
+            self._db.set(dt, text)
 
 
 if __name__ == '__main__':
