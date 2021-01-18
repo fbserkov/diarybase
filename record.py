15fce35a81ceda1e4596c82cdfeb68b73ac0c3e0
Fedor Serkov <serkovfb@gmail.com>
2021-01-18 15:55:13 +1000

    rename: text -> note

--- record.py
+++ record.py
@@ -2,10 +2,10 @@ from datetime import datetime
 
 
 class Record:
-    def __init__(self, dt=None, text=None):
+    def __init__(self, dt=None, note=None):
         self._dt = dt if dt else datetime.now()
-        self._text = text
+        self._note = note
 
     def __str__(self):
         str_dt = self._dt.strftime('%d.%m.%Y %H:%M:%S')
-        return f'[{str_dt}] ' + self._text
+        return f'[{str_dt}] ' + self._note
--- tests.py
+++ tests.py
@@ -8,8 +8,8 @@ from storage import Storage
 class TestNote(unittest.TestCase):
     def test_str(self):
         dt = datetime(year=2021, month=1, day=18, hour=11, minute=11)
-        r = Record(dt, 'Text.')
-        self.assertEqual('[18.01.2021 11:11:00] Text.', str(r))
+        r = Record(dt, 'Note.')
+        self.assertEqual('[18.01.2021 11:11:00] Note.', str(r))
 
 
 class TestStorage(unittest.TestCase):
@@ -19,25 +19,25 @@ class TestStorage(unittest.TestCase):
 
     def test_add_and_str(self):
         dt_1 = datetime(year=2021, month=1, day=16, hour=19, minute=4)
-        self._storage.append(Record(dt_1, 'First text.'))
+        self._storage.append(Record(dt_1, 'First note.'))
         dt_2 = datetime(year=2021, month=1, day=16, hour=19, minute=20)
-        self._storage.append(Record(dt_2, 'Second text.'))
+        self._storage.append(Record(dt_2, 'Second note.'))
 
         self.assertEqual(
-            '[16.01.2021 19:04:00] First text.\n'
-            '[16.01.2021 19:20:00] Second text.',
+            '[16.01.2021 19:04:00] First note.\n'
+            '[16.01.2021 19:20:00] Second note.',
             str(self._storage),
         )
 
     def test_save_and_load(self):
         dt = datetime(year=2021, month=1, day=15, hour=21, minute=13)
-        r = Record(dt, 'Text.')
+        r = Record(dt, 'Note.')
         self._storage.append(r)
         self._storage.save()
 
         storage = Storage(self._filename)
         storage.load()
-        self.assertEqual('[15.01.2021 21:13:00] Text.', str(storage))
+        self.assertEqual('[15.01.2021 21:13:00] Note.', str(storage))
 
 
 if __name__ == '__main__':
