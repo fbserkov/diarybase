98748d85c2aca12f52d8b7722e3f5168810d6ad2
Fedor Serkov <serkovfb@gmail.com>
2021-01-18 18:21:11 +1000

    Record: add ._tag_id and _is_active

--- record.py
+++ record.py
@@ -1,11 +1,19 @@
 from datetime import datetime
+from typing import Optional
+
+TAGS = {0: 'not specified'}
 
 
 class Record:
-    def __init__(self, dt=None, note=None):
+    def __init__(
+            self, dt: Optional[datetime] = None, tag_id: int = 0,
+            is_active: Optional[bool] = None, note: str = '',
+    ):
         self._dt = dt if dt else datetime.now()
+        self._tag_id = tag_id
         self._note = note
+        self._is_active = is_active
 
     def __str__(self):
         str_dt = self._dt.strftime('%d.%m.%Y %H:%M:%S')
-        return f'[{str_dt}] ' + self._note
+        return f'[{str_dt}] ({TAGS[self._tag_id]}) ' + self._note
--- tests.py
+++ tests.py
@@ -8,8 +8,8 @@ from storage import Storage
 class TestNote(unittest.TestCase):
     def test_str(self):
         dt = datetime(year=2021, month=1, day=18, hour=11, minute=11)
-        r = Record(dt, 'Note.')
-        self.assertEqual('[18.01.2021 11:11:00] Note.', str(r))
+        r = Record(dt, note='note')
+        self.assertEqual('[18.01.2021 11:11:00] (not specified) note', str(r))
 
 
 class TestStorage(unittest.TestCase):
@@ -19,25 +19,26 @@ class TestStorage(unittest.TestCase):
 
     def test_add_and_str(self):
         dt_1 = datetime(year=2021, month=1, day=16, hour=19, minute=4)
-        self._storage.append(Record(dt_1, 'First note.'))
+        self._storage.append(Record(dt_1, note='first note'))
         dt_2 = datetime(year=2021, month=1, day=16, hour=19, minute=20)
-        self._storage.append(Record(dt_2, 'Second note.'))
+        self._storage.append(Record(dt_2, note='second note'))
 
         self.assertEqual(
-            '[16.01.2021 19:04:00] First note.\n'
-            '[16.01.2021 19:20:00] Second note.',
+            '[16.01.2021 19:04:00] (not specified) first note\n'
+            '[16.01.2021 19:20:00] (not specified) second note',
             str(self._storage),
         )
 
     def test_save_and_load(self):
         dt = datetime(year=2021, month=1, day=15, hour=21, minute=13)
-        r = Record(dt, 'Note.')
+        r = Record(dt, note='note')
         self._storage.append(r)
         self._storage.save()
 
         storage = Storage(self._filename)
         storage.load()
-        self.assertEqual('[15.01.2021 21:13:00] Note.', str(storage))
+        self.assertEqual(
+            '[15.01.2021 21:13:00] (not specified) note', str(storage))
 
 
 if __name__ == '__main__':
