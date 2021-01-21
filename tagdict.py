ca633fc14e9371394c70558a0ac3cf35668e2f9e
Fedor Serkov <serkovfb@gmail.com>
2021-01-21 22:11:12 +1000

    swap set/get and load/save

--- tagdict.py
+++ tagdict.py
@@ -6,14 +6,14 @@ class TagDict:
         self._db = db
         self._tags = {}
 
-    def set_dict(self, tags: dict):
-        self._tags = tags
-
-    def get_dict(self) -> dict:
-        return self._tags
-
     def load(self):
         self._tags = self._db.load('tags')
 
     def save(self):
         self._db.save('tags', self._tags)
+
+    def set_dict(self, tags: dict):
+        self._tags = tags
+
+    def get_dict(self) -> dict:
+        return self._tags
