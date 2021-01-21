bed1fa4abb4bfac5a54146ecfa88b14e8ad5e3a3
Fedor Serkov <serkovfb@gmail.com>
2021-01-21 22:08:30 +1000

    TestTagDict.test_save_and_load (passed)

--- tagdict.py
+++ tagdict.py
@@ -1,7 +1,16 @@
+from database import Database
+
+
 class TagDict:
-    def __init__(self, db):
+    def __init__(self, db: Database):
         self._db = db
         self._tags = {}
+
+    def set_dict(self, tags: dict):
+        self._tags = tags
+
+    def get_dict(self) -> dict:
+        return self._tags
 
     def load(self):
         self._tags = self._db.load('tags')
