0cafcad29d438eacff4e53052f8672b86d7f8529
Fedor Serkov <serkovfb@gmail.com>
2021-01-30 19:05:49 +1000

    class RecordFrame

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from const import DELETE, UPDATE
+from const import DELETE, SAVE, UPDATE
 from gui_temp import TEXT, COMMAND
 from guimanager import GUIManager
 
@@ -39,42 +39,53 @@ class RecordListFrame(tk.Frame):
         self.listbox_var = tk.StringVar(frame)
         self.listbox = tk.Listbox(
             frame, width=80, listvariable=self.listbox_var)
-        self.listbox.bind('<<ListboxSelect>>', self._split_record)
+        self.listbox.bind('<<ListboxSelect>>', record_frame.split_record)
         self.listbox.pack(side=tk.LEFT)
 
         sb = tk.Scrollbar(frame, command=self.listbox.yview)
         self.listbox.configure(yscrollcommand=sb.set)
         sb.pack(side=tk.RIGHT, fill=tk.Y)
 
-        frame = tk.Frame(self)
-        frame.pack()
-
-        self._dt_var = tk.StringVar(frame)
-        self._tag_var = tk.StringVar(frame)
-        self._note_var = tk.StringVar(frame)
-        tk.Entry(frame, width=20, textvariable=self._dt_var).pack(side=tk.LEFT)
-        tk.Entry(
-            frame, width=20, textvariable=self._tag_var).pack(side=tk.LEFT)
-        tk.Entry(
-            frame, width=40, textvariable=self._note_var).pack(side=tk.LEFT)
-
         self.update_record_list()
         self.pack(side=tk.RIGHT)
-
-    def _split_record(self, event):
-        index = event.widget.curselection()[0]
-        dt, tag, note = gui_manager.split_record(index)
-        self._dt_var.set(dt)
-        self._tag_var.set(tag)
-        self._note_var.set(note)
 
     def update_record_list(self):
         self.listbox_var.set(gui_manager.str_record_list())
         self.listbox.yview_moveto(fraction=1)
 
 
+class RecordFrame(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        frame = tk.Frame(self)
+        frame.pack(side=tk.LEFT)
+
+        self._dt_var = tk.StringVar(frame)
+        self._tag_var = tk.StringVar(frame)
+        self._note_var = tk.StringVar(frame)
+        tk.Entry(frame, width=20, textvariable=self._dt_var).pack()
+        tk.Entry(
+            frame, width=20, textvariable=self._tag_var).pack()
+        tk.Button(frame, text=SAVE).pack()
+
+        frame = tk.Frame(self)
+        frame.pack(side=tk.RIGHT)
+        tk.Entry(
+            frame, width=40, textvariable=self._note_var).pack()
+
+        self.pack(side=tk.BOTTOM)
+
+    def split_record(self, event):
+        index = event.widget.curselection()[0]
+        dt, tag, note = gui_manager.split_record(index)
+        self._dt_var.set(dt)
+        self._tag_var.set(tag)
+        self._note_var.set(note)
+
+
 root = tk.Tk()
 gui_manager = GUIManager()
+record_frame = RecordFrame(root)
 record_list_frame = RecordListFrame(root)
 MenuFrame(root)
 root.mainloop()
