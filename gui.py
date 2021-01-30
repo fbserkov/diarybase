364c605325f43a3d9724f4e0446cc27ea61696e4
Fedor Serkov <serkovfb@gmail.com>
2021-01-30 19:18:22 +1000

    for Record._note: tk.Entry -> tk.Text

--- gui.py
+++ gui.py
@@ -15,6 +15,7 @@ def add_update_call(to_wrap):
 class MenuFrame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+        self.pack(side=tk.LEFT)
 
         tk.Button(self, text=TEXT, command=add_update_call(COMMAND)).pack()
         for t, c in gui_manager.text_and_command_list:
@@ -27,27 +28,24 @@ class MenuFrame(tk.Frame):
             self, text=UPDATE,
             command=record_list_frame.update_record_list,
         ).pack()
-        self.pack(side=tk.LEFT)
 
 
 class RecordListFrame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+        self.pack(side=tk.RIGHT)
+
         frame = tk.Frame(self)
         frame.pack()
-
         self.listbox_var = tk.StringVar(frame)
         self.listbox = tk.Listbox(
             frame, width=80, listvariable=self.listbox_var)
         self.listbox.bind('<<ListboxSelect>>', record_frame.split_record)
         self.listbox.pack(side=tk.LEFT)
-
         sb = tk.Scrollbar(frame, command=self.listbox.yview)
         self.listbox.configure(yscrollcommand=sb.set)
         sb.pack(side=tk.RIGHT, fill=tk.Y)
-
         self.update_record_list()
-        self.pack(side=tk.RIGHT)
 
     def update_record_list(self):
         self.listbox_var.set(gui_manager.str_record_list())
@@ -57,30 +55,28 @@ class RecordListFrame(tk.Frame):
 class RecordFrame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+        self.pack(side=tk.BOTTOM)
+
         frame = tk.Frame(self)
         frame.pack(side=tk.LEFT)
-
         self._dt_var = tk.StringVar(frame)
         self._tag_var = tk.StringVar(frame)
-        self._note_var = tk.StringVar(frame)
         tk.Entry(frame, width=20, textvariable=self._dt_var).pack()
-        tk.Entry(
-            frame, width=20, textvariable=self._tag_var).pack()
-        tk.Button(frame, text=SAVE).pack()
+        tk.Entry(frame, width=20, textvariable=self._tag_var).pack()
 
         frame = tk.Frame(self)
-        frame.pack(side=tk.RIGHT)
-        tk.Entry(
-            frame, width=40, textvariable=self._note_var).pack()
-
-        self.pack(side=tk.BOTTOM)
+        frame.pack(side=tk.LEFT)
+        self._text = tk.Text(frame, width=40, height=4)
+        self._text.pack(side=tk.LEFT)
+        tk.Button(frame, text=SAVE).pack(side=tk.LEFT)
 
     def split_record(self, event):
         index = event.widget.curselection()[0]
         dt, tag, note = gui_manager.split_record(index)
         self._dt_var.set(dt)
         self._tag_var.set(tag)
-        self._note_var.set(note)
+        self._text.delete('1.0', tk.END)
+        self._text.insert('1.0', note)
 
 
 root = tk.Tk()
