acd16c5f0e7df0c29a60d91c75f0d3014d1144a9
Fedor Serkov <serkovfb@gmail.com>
2021-01-30 12:59:46 +1000

    split GUI class: ListboxFrame and ButtonFrame

--- gui.py
+++ gui.py
@@ -4,62 +4,63 @@ from const import DELETE, UPDATE
 from gui_temp import TEXT, COMMAND
 from guimanager import GUIManager
 
+gui_manager = GUIManager()
 
-class GUI(tk.Frame):
+
+def add_update_call(to_wrap):
+    def wrapper():
+        to_wrap()
+        lbf.update_record_list()
+    return wrapper
+
+
+class ButtonFrame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        self._gui_manager = GUIManager()
 
-        self._listbox_var = tk.StringVar(master=self)
-        self._listbox = tk.Listbox(
-            master=self, width=80, listvariable=self._listbox_var)
-        self._entry_var = tk.StringVar(master=self)
-        self._entry = tk.Entry(
-            master=self, width=80, textvariable=self._entry_var)
-
-        self._create_widgets()
-        self._update_record_list()
-        self.pack()
-
-    def _create_widgets(self):
         tk.Button(
-            master=self, text=TEXT,
-            command=self._add_update_call(COMMAND),
+            master=self, text=TEXT, command=add_update_call(COMMAND),
         ).pack()
-        for t, c in self._gui_manager.text_and_command_list:
-            tk.Button(master=self, text=t, command=self._add_update_call(c)).pack()
-
+        for t, c in gui_manager.text_and_command_list:
+            tk.Button(
+                master=self, text=t, command=add_update_call(c)).pack()
         tk.Button(
             master=self, text=DELETE,
-            command=self._add_update_call(self._delete_last_record),
+            command=add_update_call(gui_manager.delete_last_record),
         ).pack()
         tk.Button(
-            master=self, text=UPDATE,
-            command=self._update_record_list,
-        ).pack()
+            master=self, text=UPDATE, command=lbf.update_record_list).pack()
 
-        self._listbox.bind('<<ListboxSelect>>', self._listbox_callback)
-        self._listbox.pack()
-        self._entry.pack()
 
-    def _add_update_call(self, to_wrap):
-        def wrapper():
-            to_wrap()
-            self._update_record_list()
-        return wrapper
+class ListboxFrame(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+
+        self.listbox_var = tk.StringVar(master=self)
+        self.listbox = tk.Listbox(
+            master=self, width=80, listvariable=self.listbox_var)
+        self.listbox.bind('<<ListboxSelect>>', self._listbox_callback)
+        self.listbox.pack()
+
+        self.entry_var = tk.StringVar(master=self)
+        self.entry = tk.Entry(
+            master=self, width=80, textvariable=self.entry_var)
+        self.entry.pack()
+
+        self.update_record_list()
 
     def _listbox_callback(self, event):
         index = event.widget.curselection()[0]
-        self._entry_var.set(self._gui_manager.get_record(index))
+        self.entry_var.set(gui_manager.get_record(index))
 
-    def _delete_last_record(self):
-        self._gui_manager.delete_last_record()
-
-    def _update_record_list(self):
-        self._listbox_var.set(self._gui_manager.str_record_list())
-        self._listbox.yview_moveto(1)
+    def update_record_list(self):
+        self.listbox_var.set(gui_manager.str_record_list())
+        self.listbox.yview_moveto(1)
 
 
 root = tk.Tk()
-GUI(master=root)
+lbf = ListboxFrame(master=root)
+bf = ButtonFrame(master=root)
+bf.pack()
+lbf.pack()
 root.mainloop()
