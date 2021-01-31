e41adfad4e99ab401e10dd972f4a59105f094a09
Fedor Serkov <serkovfb@gmail.com>
2021-02-01 09:26:51 +1000

    gui.py: IsActiveFrame

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from typing import Optional
 
 from const import DELETE, SAVE, UPDATE
 from guimanager import GUIManager
@@ -61,6 +62,8 @@ class RecordFrame(tk.Frame):
         self._tag_var = tk.StringVar(frame)
         tk.Entry(frame, width=20, textvariable=self._dt_var).pack()
         tk.Entry(frame, width=20, textvariable=self._tag_var).pack()
+        self.is_active_frame = IsActiveFrame(frame)
+        self.is_active_frame.pack()
 
         frame = tk.Frame(self)
         frame.pack(side=tk.LEFT)
@@ -75,26 +78,62 @@ class RecordFrame(tk.Frame):
         curselection = event.widget.curselection()
         if not curselection:
             return
-        dt, tag, note = gui_manager.split_record(curselection[0])
+        dt, tag, is_active, note = gui_manager.split_record(curselection[0])
         self._dt_var.set(dt)
         self._tag_var.set(tag)
+        self.is_active_frame.set(is_active)
         self._text.delete('1.0', tk.END)
         self._text.insert('1.0', note)
 
     def _save_record(self):
         curselection = record_list_frame.listbox.curselection()
         if curselection:
-            if not gui_manager.update_record(
-                    index=curselection[0],
-                    str_dt=self._dt_var.get(),
-                    note=self._text.get('1.0', tk.END + '-1c'),
-            ):
+            error = not gui_manager.update_record(
+                index=curselection[0],
+                str_dt=self._dt_var.get(),
+                tag=self._tag_var.get(),
+                is_active=self.is_active_frame.get(),
+                note=self._text.get('1.0', tk.END + '-1c'),
+            )
+            if error:
                 self._text.insert('1.0', 'ValueError\n')
         else:
             gui_manager.add_record(
                 tag=self._tag_var.get(),
                 note=self._text.get('1.0', tk.END + '-1c'),
             )
+
+
+class IsActiveFrame(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.start_var = tk.IntVar(self)
+        self.end_var = tk.IntVar(self)
+        tk.Checkbutton(
+            self, text='start', variable=self.start_var,
+            command=lambda: self.end_var.set(0),
+        ).pack(side=tk.LEFT)
+        tk.Checkbutton(
+            self, text='end', variable=self.end_var,
+            command=lambda: self.start_var.set(0),
+        ).pack(side=tk.LEFT)
+
+    def set(self, is_active: Optional[bool]):
+        if is_active:
+            self.start_var.set(1)
+            self.end_var.set(0)
+        elif is_active is None:
+            self.start_var.set(0)
+            self.end_var.set(0)
+        else:
+            self.start_var.set(0)
+            self.end_var.set(1)
+
+    def get(self) -> Optional[bool]:
+        if self.start_var.get():
+            return True
+        if self.end_var.get():
+            return False
 
 
 root = tk.Tk()
